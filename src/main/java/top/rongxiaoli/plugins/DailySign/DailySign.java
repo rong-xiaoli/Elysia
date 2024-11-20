ad8fe84ee51040c6140841f3555232aa4a058512
容小狸 <hashnoob079@163.com>
2024-11-20 18:43:13 +0800

    Sync.

--- src/main/java/top/rongxiaoli/plugins/DailySign/DailySign.java
+++ src/main/java/top/rongxiaoli/plugins/DailySign/DailySign.java
@@ -1,14 +1,29 @@
 package top.rongxiaoli.plugins.DailySign;
 
+import net.mamoe.mirai.console.command.java.JSimpleCommand;
+import net.mamoe.mirai.utils.MiraiLogger;
+import top.rongxiaoli.Elysia;
 import top.rongxiaoli.backend.PluginBase;
 
-public class DailySign implements PluginBase {
+public class DailySign extends JSimpleCommand implements PluginBase {
+    private static final DailySignData DATA = new DailySignData();
+    private static final MiraiLogger logger = MiraiLogger.Factory.INSTANCE.create(DailySign.class);
+    public static final DailySign INSTANCE = new DailySign();
+    public DailySign() {
+        super(Elysia.INSTANCE, "sign", "qd");
+        setDescription("每日签到");
+    }
+
     /**
      * Load method. First time loading.
      */
     @Override
     public void load() {
-
+        logger.debug("DailySign loading. ");
+        DATA.load();
+        logger.verbose("Data load complete. ");
+        logger.verbose("No config load needed. ");
+        logger.debug("DailySign loaded. ");
     }
 
     /**
@@ -16,7 +31,11 @@ public class DailySign implements PluginBase {
      */
     @Override
     public void reload() {
-
+        logger.debug("DailySign reloading. ");
+        DATA.reload();
+        logger.verbose("Data load complete. ");
+        logger.verbose("No config reload needed. ");
+        logger.debug("DailySign reloaded. ");
     }
 
     /**
@@ -24,7 +43,11 @@ public class DailySign implements PluginBase {
      */
     @Override
     public void shutdown() {
-
+        logger.debug("DailySign shutting down. ");
+        DATA.shutdown();
+        logger.verbose("Data shutdown complete. ");
+        logger.verbose("No config shutdown needed. ");
+        logger.debug("DailySign shut down. ");
     }
 
     /**
@@ -32,7 +55,9 @@ public class DailySign implements PluginBase {
      */
     @Override
     public void saveData() {
-
+        logger.debug("DailySign reloading. ");
+        DATA.saveData();
+        logger.verbose("Data saved. ");
     }
 
     /**
@@ -40,6 +65,10 @@ public class DailySign implements PluginBase {
      */
     @Override
     public void reloadData() {
-
+        logger.debug("DailySign data reloading. ");
+        DATA.reload();
+        logger.verbose("Data reload complete. ");
+        logger.verbose("No config needed. ");
+        logger.debug("DailySign reloaded. ");
     }
 }
--- src/main/java/top/rongxiaoli/plugins/DailySign/DailySignData.java
+++ src/main/java/top/rongxiaoli/plugins/DailySign/DailySignData.java
@@ -1,28 +1,54 @@
 package top.rongxiaoli.plugins.DailySign;
 
+import net.mamoe.mirai.console.data.Value;
 import net.mamoe.mirai.console.data.java.JavaAutoSavePluginData;
+import net.mamoe.mirai.utils.MiraiLogger;
+import top.rongxiaoli.Elysia;
 import top.rongxiaoli.backend.PluginDataBase;
+
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DailySignData extends JavaAutoSavePluginData implements PluginDataBase {
     public static final DailySignData INSTANCE = new DailySignData();
-    private static final String NAME = "DailySign.Data";
+    private static final MiraiLogger LOGGER = MiraiLogger.Factory.INSTANCE.create(DailySignData.class);
+    public class DailySignPersonData {
+        public GregorianCalendar lastLoginDate;
+        public int ContinuousSignCombo;
+    }
     public DailySignData() {
         super("DailySignData");
     }
-
+    public final Value<Map<Long, DailySignPersonData>> DailySignDataSet = typedValue("DailySignDataSet",
+            createKType(Map.class, createKType(Long.class), createKType(DailySignPersonData.class,
+                    createKType(GregorianCalendar.class), createKType(int.class))),
+            new HashMap<>());
     @Override
     public void load() {
+        LOGGER.verbose("Loading data. ");
+        Elysia.INSTANCE.reloadPluginData(INSTANCE);
+        LOGGER.debug("Load complete. ");
     }
 
     @Override
     public void reload() {
+        LOGGER.debug("Start reloading data. ");
+        Elysia.INSTANCE.reloadPluginData(INSTANCE);
+        LOGGER.debug("Data reloading complete. ");
     }
 
     @Override
     public void shutdown() {
+        LOGGER.debug("Start shutdown process. ");
+        Elysia.INSTANCE.savePluginData(INSTANCE);
+        LOGGER.debug("Shutdown process complete. ");
     }
 
     @Override
     public void saveData() {
+        LOGGER.debug("Saving data. ");
+        Elysia.INSTANCE.savePluginData(INSTANCE);
+        LOGGER.debug("Data saved. ");
     }
 }
