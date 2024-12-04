8416aa9e3008535bba8b80e342d0bf18ddb00603
容小狸 <hashnoob079@163.com>
2024-12-05 03:23:22 +0800

    Sync.

---
+++ src/main/java/top/rongxiaoli/plugins/PokeReact/backend/PokeBack.java
@@ -0,0 +1,18 @@
+package top.rongxiaoli.plugins.PokeReact.backend;
+
+import net.mamoe.mirai.console.command.descriptor.CommandArgumentContext;
+import net.mamoe.mirai.utils.MiraiLogger;
+import top.rongxiaoli.ArisuBot;
+
+import java.util.Random;
+
+public class PokeBack {
+    MiraiLogger LOGGER = MiraiLogger.Factory.INSTANCE.create(PokeBack.class, "ArisuBot.PokeReact.PokeBack");
+    private static void getRandomString() {
+        Random random = new Random(System.currentTimeMillis());
+        
+    }
+    public static void React(CommandArgumentContext context) {
+
+    }
+}
